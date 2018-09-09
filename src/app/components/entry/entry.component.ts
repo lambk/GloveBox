6bc8bab1b2323ba5f0909c543d06ffb5c4616121
lambk <kyle.lamb@hotmail.co.nz>
2018-09-09 22:37:50 +1200

    Added alert on successful registration. Moved form feedback from registration component to entry component

--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -1,26 +1,51 @@
 import { Component } from '@angular/core';
 import { RegisterFormComponent } from '../register-form/register-form.component';
 import { Subject } from 'rxjs';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { AlertType } from '../../constants';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.css']
+  styleUrls: ['./entry.component.css'],
+  animations: [
+    trigger('alertFade', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateY(-10px)'}),
+        animate(500, style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        }))
+      ]),
+      transition(':leave', [
+        animate(500, style({
+          opacity:0,
+          transform: 'translateY(-10px)'
+        }))
+      ])
+    ])
+  ]
 })
 export class EntryComponent {
 
-  private registerSubmitSubject : Subject<void>
+  private registerSubmitSubject: Subject<void>
 
-  private collapsed = {
+  private collapsed: any = {
     login: true,
     register: true
   };
 
+  private alertdata: any = {
+    msg: undefined,
+    type: undefined,
+    show: false
+  };
+
   constructor() {
-    this.registerSubmitSubject = new Subject<void>()
+    this.registerSubmitSubject = new Subject<void>();
   }
 
-  onRegisterClick() {
+  onRegisterClick(): void {
     if (this.collapsed.register) {
       this.collapsed.register = false;
     } else {
@@ -28,7 +53,16 @@ export class EntryComponent {
     }
   }
 
-  onLoginClick() {
-    
+  onRegisterSubmit(event): void {
+    if (event.successful) {
+      this.collapsed.register = true;
+    }
+    this.alertdata.msg = event.feedback.msg;
+    this.alertdata.type = event.feedback.type;
+    this.alertdata.show = true;
+  }
+
+  onLoginClick(): void {
+
   }
 }
--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,42 +1,21 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subject } from 'rxjs';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 import { RegisterService } from '../../services/register.service';
+import { AlertType } from '../../constants';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css'],
-  animations: [
-    trigger('errorFade', [
-      transition(':enter', [
-        style({opacity: 0, transform: 'translateY(-10px)'}),
-        animate(500, style({
-          opacity: 1,
-          transform: 'translateY(0)'
-        }))
-      ]),
-      transition(':leave', [
-        animate(500, style({
-          opacity:0,
-          transform: 'translateY(-10px)'
-        }))
-      ])
-    ])
-  ]
+  styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
 
   @Input() submitSubject : Subject<void>;
-  @Output() onComplete = new EventEmitter;
+  @Output() onSubmit = new EventEmitter;
 
   @ViewChild(NgForm) form;
   data: any = {};
-  error: any = {
-    msg: undefined,
-    show: false
-  };
 
   constructor(private registerService: RegisterService) { }
 
@@ -58,16 +37,25 @@ export class RegisterFormComponent implements OnInit {
       },
       password: this.data.password
     };
-    this.registerService.registerUser(formdata).then((res) => {
+    this.registerService.registerUser(formdata).then((response) => {
       //Reset the form
-      this.error.show = false;
       this.form.submitted = false;
       this.form.form.reset();
-      //Emit completion event so the entry component can collapse the form
-      this.onComplete.emit();
+      this.onSubmit.emit({
+        successful: true,
+        feedback: {
+          msg: response,
+          type: AlertType.SUCCESS
+        }
+      });
     }, (err) => {
-      this.error.msg = err.error;
-      this.error.show = true;
+      this.onSubmit.emit({
+        successful: false,
+        feedback: {
+          msg: err.error,
+          type: AlertType.ERROR
+        }
+      });
     });
   }
 }
