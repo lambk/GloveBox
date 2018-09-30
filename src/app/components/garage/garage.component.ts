8eff323aa3dada34c9ac938bdd23c5ed6ed0a197
lambk <kyle.lamb@hotmail.co.nz>
2018-10-01 12:43:33 +1300

    Linked garage table to a vehicle object in the component

--- src/app/components/garage/garage.component.ts
+++ src/app/components/garage/garage.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GarageComponent implements OnInit {
 
+  public vehicles = [
+    {
+      plate: 'ABC123',
+      make: 'Toyota',
+      model: 'Celica',
+      year: 1994
+    },
+    {
+      plate: 'HHH111',
+      make: 'Honda',
+      model: 'Integra',
+      year: 1994
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
