309cff4604c3f4fada01011cbc6161f14af08374
lambk <kyle.lamb@hotmail.co.nz>
2018-10-25 20:49:56 +1300

    User vehicles are now stored in the service class so there are no repeated ajax calls when navigating back to the garage

--- src/app/services/vehicle/vehicle.service.ts
+++ src/app/services/vehicle/vehicle.service.ts
@@ -1,31 +1,36 @@
+import { Observable, of } from 'rxjs';
 import { Vehicle } from './../../interfaces/vehicle.model';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { VehicleRegistrationDTO } from 'src/app/interfaces/vehicleRegistration.dto';
 import { environment } from 'src/environments/environment';
 
+const headers = new HttpHeaders({
+  'token': localStorage.getItem('token'),
+  'Content-Type': 'application/json'
+});
+
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
 
+  private vehicles: Vehicle[];
+
   constructor(private http: HttpClient) { }
 
   register(vehicle: VehicleRegistrationDTO) {
-    const headers = new HttpHeaders({
-      'token': localStorage.getItem('token'),
-      'Content-Type': 'application/json'
-    });
     return this.http.post(`${environment.server_url}/vehicles/${localStorage.getItem('id')}`,
       vehicle, {headers: headers, responseType: 'text'});
   }
 
   getAll() {
-    const headers = new HttpHeaders({
-      'token': localStorage.getItem('token'),
-      'Content-Type': 'application/json'
-    });
-    return this.http.get<Vehicle[]>(`${environment.server_url}/vehicles/${localStorage.getItem('id')}`,
+    if (this.vehicles) {
+      return of(this.vehicles);
+    }
+    const ajax = this.http.get<Vehicle[]>(`${environment.server_url}/vehicles/${localStorage.getItem('id')}`,
       {headers: headers, responseType: 'json'});
+    ajax.subscribe((res) => this.vehicles = res);
+    return ajax;
   }
 }
