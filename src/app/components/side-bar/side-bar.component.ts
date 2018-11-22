6d049e48de86242f982338fdd68c33be75189ed7
lambk <kyle.lamb@hotmail.co.nz>
2018-11-22 18:08:58 +1300

    Added custom sidebar color support. Can currently only be changed using localstorage

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
+const defaultThemeColor = '212F3D';
+
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -10,6 +12,7 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class SideBarComponent implements OnInit {
   public href = '';
+  public themeColor;
 
   constructor(private router: Router, private authService: AuthService, private loadingService: LoadingService) { }
 
@@ -19,6 +22,11 @@ export class SideBarComponent implements OnInit {
         this.href = e.url;
       }
     });
+    if (/^[0-9A-F]{6}$/.test(localStorage.getItem('theme'))) {
+      this.themeColor = localStorage.getItem('theme');
+    } else {
+      this.themeColor = defaultThemeColor;
+    }
   }
 
   logout(): void {
